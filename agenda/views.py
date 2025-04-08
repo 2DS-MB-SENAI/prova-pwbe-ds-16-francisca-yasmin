f6da0a71b883e63fc5d9c6c8f48ea4ace64a750a
Francisca Yasmin Freitas de Almeida <47915176870@>
2025-04-08 11:10:58 -0300

    ?

--- agenda/views.py
+++ agenda/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from requests import Response
+from .serializers import ServicoSerializer
 from .models import Servico, Agendamento
+from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+@api_view(['GET']) #listar informações
 def read_servicos(request):
-    servicos =1
+    servicos = Servico.objects.all()
+    serializer = ServicoSerializer(servicos, many=True) #nome da variavel que criamos
+    return Response(serializer.data) #data -> dados
+
+
+
