47f01c87f8c977c0f3d9a52f98db95718e0b228c
Francisca Yasmin Freitas de Almeida <47915176870@>
2025-04-08 11:16:10 -0300

    ?

--- agenda/urls.py
+++ agenda/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/servicos', views.read_servicos),
+    path('api/servicos/', views.read_servicos),
+    path('/api/servicos/', views.create_servicos),
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -3,6 +3,7 @@ from requests import Response
 from .serializers import ServicoSerializer
 from .models import Servico, Agendamento
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 @api_view(['GET']) #listar informações
 def read_servicos(request):
@@ -10,5 +11,23 @@ def read_servicos(request):
     serializer = ServicoSerializer(servicos, many=True) #nome da variavel que criamos
     return Response(serializer.data) #data -> dados
 
+@api_view(['POST'])
+def create_servicos(request):
+     if request.method == 'POST':
+        serializer = ServicoSerializer(data=request.data, many=isinstance(request.data, list)) #converter o JSON para carro
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['GET']) #pegar informações de serviços oferecidos
+def pegar_servico(request, pk):
+    try:
+        servicos = Servico.objects.get(pk = pk)
+    except Servico.DoesNotExist:
+        return Response({'erro': ' serviço inexistente'}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = ServicoSerializer(servicos)
+    return Response(serializer.data)
 
